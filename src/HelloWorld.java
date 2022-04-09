f44c06767df36d80c4723dc7fbe46234c104e4cb
3emeJI9I <TJNew@yandex.ru>
2022-04-09 10:49:01 +0500

    Hello

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * A program that prints <code>Hello world!</code>
+ *
+ * @author Evgeniy Sokolovskiy
+ * @version 1.0
+ */
+public class HelloWorld {
+    /**
+     * Program entry point.
+     *
+     * @param cmdLineArgs command-line arguments
+     */
+    public static void main(String[] cmdLineArgs) {
+        System.out.println("Hello,World!");
+    }
+}
